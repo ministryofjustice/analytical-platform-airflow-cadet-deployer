5300d9b008d3136c8d113b06ad2263563716276c
Jacob Woffenden <jacob@woffenden.io>
2025-07-21 22:18:44 +0000

    Enhance scripts with improved logging and retry mechanism for dbt execution
    Signed-off-by: GitHub <noreply@github.com>

--- src/clone-create-a-derived-table.sh
+++ src/clone-create-a-derived-table.sh
@@ -8,12 +8,21 @@ export GITHUB_KEY="${SECRET_GITHUB_KEY}"
 export GITHUB_KEY_PATH="${GITHUB_KEY_PATH:-"/tmp/github-key"}"
 export REPOSITORY_PATH="${REPOSITORY_PATH:-"${ANALYTICAL_PLATFORM_DIRECTORY}/create-a-derived-table"}"
 
-echo "Writing GitHub deploy key to ${GITHUB_KEY_PATH}"
+echo "Writing GitHub deploy key to [ ${GITHUB_KEY_PATH} ]"
 mkdir --parents "$(dirname "${GITHUB_KEY_PATH}")"
 
-echo "${GITHUB_KEY}" | base64 --decode > "${GITHUB_KEY_PATH}"
+echo "${GITHUB_KEY}" | base64 --decode >"${GITHUB_KEY_PATH}"
 
 chmod 0600 "${GITHUB_KEY_PATH}"
 
-echo "Cloning repository ${REPOSITORY} on branch ${BRANCH}"
+echo "Cloning repository [ ${REPOSITORY} ] on branch [ ${BRANCH} ]"
 GIT_SSH_COMMAND="ssh -i ${GITHUB_KEY_PATH} -o UserKnownHostsFile=ssh-known-hosts" git clone --branch "${BRANCH}" "${REPOSITORY}" "${REPOSITORY_PATH}"
+
+cd "${REPOSITORY_PATH}"
+
+currentCommit=$(git rev-parse HEAD)
+export currentCommit
+
+commitActor=$(git log -1 --pretty=format:'%an <%ae>')
+
+echo "HEAD commit SHA [ ${currentCommit} ] by actor [ ${commitActor} ]"
--- src/create-a-derived-table.sh
+++ src/create-a-derived-table.sh
@@ -7,14 +7,35 @@ export MODE="${MODE}"
 export DBT_PROFILES_DIR="${DBT_PROFILES_DIR:-"${REPOSITORY_PATH}/.dbt"}"
 export DBT_PROJECT="${DBT_PROJECT}"
 export DEPLOY_ENV="${DEPLOY_ENV}"
+export DBT_SELECT_CRITERIA="${DBT_SELECT_CRITERIA}"
 
-echo "Running in mode [ ${MODE} ] for project [ ${DBT_PROJECT} ]"
+function run_dbt() {
+  local max_retries=5
+  local attempt=1
+
+  # Disable immediate exit on error for the loop
+  set +e
+  while [[ "${attempt}" -le "${max_retries}" ]]; do
+    echo "Attempt ${attempt} of ${max_retries} to run dbt command"
+    if dbt "${MODE}" --select "${DBT_SELECT_CRITERIA}" --target "${DEPLOY_ENV}"; then
+      echo "dbt command succeeded on attempt ${attempt}"
+      break
+    elif [[ "${attempt}" -eq "${max_retries}" ]]; then
+      echo "dbt command failed after ${max_retries} attempts. Exiting."
+      exit 1
+    else
+      echo "dbt command failed on attempt ${attempt}. Retrying..."
+      ((attempt++))
+      sleep 5  # Wait before retrying
+    fi
+}
 
 echo "Creating virtual environment and installing dependencies"
 cd "${REPOSITORY_PATH}"
 
 uv venv
 
+# shellcheck disable=SC1091
 source .venv/bin/activate
 
 uv pip install --requirements requirements.txt
@@ -33,3 +54,6 @@ dbt clean
 
 echo "Running dbt deps"
 dbt deps
+
+echo "Running in mode [ ${MODE} ] for project [ ${DBT_PROJECT} ] to environment [ ${DEPLOY_ENV} ] with select criteria [ ${DBT_SELECT_CRITERIA} ]"
+run_dbt
