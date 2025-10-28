5135de6f625d1ead52fb63c89ee02e4a61b596d5
Jake H-P <72385707+jhpyke@users.noreply.github.com>
2025-10-28 16:44:50 +0000

    Add diagnostic print to image
    Print the values we're getting to understand why we're not hitting loop

--- src/create-a-derived-table.sh
+++ src/create-a-derived-table.sh
@@ -88,6 +88,10 @@ if [ "$DBT_PROJECT" = "hmpps_electronic_monitoring_data_tables" ]; then
   python3 "${REPOSITORY_PATH}/${DBT_PROJECT}/scripts/environment.py"
   # shellcheck source=entrypoint.sh
   source "${REPOSITORY_PATH}/${DBT_PROJECT}/set_env.sh"
+  echo "em_remove_historic is set to $EM_REMOVE_HISTORIC"
+  echo [ "$EM_REMOVE_HISTORIC" = "true" ]
+  echo "em_remove_live is set to $EM_REMOVE_LIVE"
+  echo [ "$EM_REMOVE_LIVE" = "true" ]
   if [ "$EM_REMOVE_HISTORIC" = "true" ]; then
     echo "Removing historic models for EM..."
     rm -rf "${REPOSITORY_PATH}/${DBT_PROJECT}/models/historic"
