da4c722d13b68fc48420fbdc7cd7b8675347f560
Jake H-P <jacob.hamblin-pyke@justice.gov.uk>
2025-11-17 11:03:58 +0000

    Update exit handling to better match github

--- src/create-a-derived-table.sh
+++ src/create-a-derived-table.sh
@@ -19,6 +19,7 @@ export EM_REMOVE_LIVE="${EM_REMOVE_LIVE:-false}"
 function run_dbt() {
   local max_retries=5
   local attempt=2
+  local run_results_exists=0
 
   # Disable immediate exit on error for the loop
   set +e
@@ -31,11 +32,17 @@ function run_dbt() {
   while [[ "${attempt}" -le "${max_retries}" ]]; do
     echo "Attempt ${attempt} of ${max_retries} to run dbt command"
     if [[ "${attempt}" -eq "${max_retries}" ]]; then
-      echo "dbt command failed after ${max_retries} attempts"
-      return 1
+      if run_results_exists=0; then
+        echo "dbt command failed after ${max_retries} attempts"
+        return 1
+      else
+        echo "dbt command at least partially succeeded, see run artefacts for details"
+        return 0
+      fi
     else
       echo "dbt command failed on attempt ${attempt}, retrying"
       if [[ -f "${REPOSITORY_PATH}/${DBT_PROJECT}/target/run_results.json" ]]; then
+        run_results_exists=1
         echo "run_results.json exists, retrying"
         if dbt retry; then
           echo "dbt retry succeeded"
