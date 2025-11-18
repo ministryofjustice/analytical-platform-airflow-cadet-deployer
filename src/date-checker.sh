0270d8edd92e74198d3c81c20834c91a3f2d704d
Jake H-P <jacob.hamblin-pyke@justice.gov.uk>
2025-11-18 12:48:57 +0000

    Add handling to cancel weeklies on monthly run

--- src/date-checker.sh
+++ src/date-checker.sh
@@ -5,9 +5,13 @@ weekday=$(date +%w)
 day=$(date +%d)
 
 if [ "$weekday" -eq 0 ] && [ "$day" -le 7 ]; then
-  echo "Today is the first Sunday of the month, monthly deploys will run."
+  echo "Today is the first Sunday of the month, monthly deploys will run and weeklies will error."
+  if [[ ! "$DBT_SELECT_CRITERIA" == *tag:monthly* ]]; then
+    echo "DBT_SELECT_CRITERIA does NOT contain 'tag:monthly'"
+    exit 1
+  fi
 else
-  echo "Today is NOT the first Sunday of the month, monthly deploys will be skipped."
+  echo "Today is NOT the first Sunday of the month, monthly deploys will error and weeklies will continue."
   if [[ "$DBT_SELECT_CRITERIA" == *tag:monthly* ]]; then
     echo "DBT_SELECT_CRITERIA contains 'tag:monthly', exiting with error."
     exit 1
