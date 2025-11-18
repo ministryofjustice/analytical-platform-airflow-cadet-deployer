a6f1af7bdb97b4ee9a6816674111333297b8dca3
Jake H-P <72385707+jhpyke@users.noreply.github.com>
2025-11-18 13:05:29 +0000

    Adding a date-checking step because `cron` can't handle that (#34)
    * Adding a date-checking step because `cron` can't handle that
    
    * Add handling to cancel weeklies on monthly run
    
    * Making tag generic to handle other monthly/weekly deploys
    
    * Add `-euo pipefail` statement
    
    * Make shellscript executable

---
+++ src/date-checker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Get day of week (0=Sunday), day of month, and month/year
+weekday=$(date +%w)
+day=$(date +%d)
+
+if [ "$weekday" -eq 0 ] && [ "$day" -le 7 ]; then
+  echo "Today is the first Sunday of the month, monthly deploys will run and weeklies will error."
+  if [[ ! "$DBT_SELECT_CRITERIA" == *monthly* ]]; then
+    echo "DBT_SELECT_CRITERIA does NOT contain 'tag:monthly'"
+    exit 1
+  fi
+else
+  echo "Today is NOT the first Sunday of the month, monthly deploys will error and weeklies will continue."
+  if [[ "$DBT_SELECT_CRITERIA" == *monthly* ]]; then
+    echo "DBT_SELECT_CRITERIA contains 'tag:monthly', exiting with error."
+    exit 1
+  fi
+fi
