252b85badce6652d8373bc36fd2fea5d9b27f612
Jake H-P <jacob.hamblin-pyke@justice.gov.uk>
2026-01-16 15:35:20 +0000

    Update entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 export IS_SUNDAY_DEPLOY="${IS_SUNDAY_DEPLOY:-false}"
 export MOJAP_IMAGE_VERSION="${MOJAP_IMAGE_VERSION:-"unknown"}"
+export IS_DATASET_CHECK="${IS_DATASET_CHECK:-default}"
+export DATASET_TARGET="${DATASET_TARGET:-""}"
 
 echo "=== Running Airflow CaDeT Deployer (version: ${MOJAP_IMAGE_VERSION}) ==="
 
@@ -11,6 +13,11 @@ echo "=== Is Sunday Deploy: ${IS_SUNDAY_DEPLOY} ==="
 if [ "${IS_SUNDAY_DEPLOY}" = "True" ]; then
   echo "=== Checking if today is the first Sunday of the month ==="
   ./date-checker.sh
+fi
+
+echo "=== Am I running a dataset check? ${IS_DATASET_CHECK} ==="
+if [ "${IS_DATASET_CHECK}" = "True" ]; then
+  uv run check_run_results.py
 fi
 
 echo "=== Running clone-create-a-derived-table.sh ==="
