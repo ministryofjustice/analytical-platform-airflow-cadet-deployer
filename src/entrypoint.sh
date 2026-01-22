a51a511f9b04ab53048248712fe7b52b7f80dc2f
Jake H-P <72385707+jhpyke@users.noreply.github.com>
2026-01-22 13:52:46 +0000

    Further run results parsing enhancements (#58)
    * Update script to expect yaml in cloned repo
    
    * Re-order entrypoint to ensure we always clone repo before kicking into our follow-on job
    
    * Add logging for if yaml can't be found at expected path
    
    * chore: docstring
    
    * Simplify yaml parsing
    
    * Disable ruff scanning
    
    * unbreak action

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 export IS_SUNDAY_DEPLOY="${IS_SUNDAY_DEPLOY:-false}"
 export MOJAP_IMAGE_VERSION="${MOJAP_IMAGE_VERSION:-"unknown"}"
-export IS_DATASET_CHECK="${IS_DATASET_CHECK:-default}"
+export IS_DATASET_CHECK="${IS_DATASET_CHECK:-"False"}"
 export DATASET_TARGET="${DATASET_TARGET:-""}"
 
 echo "=== Running Airflow CaDeT Deployer (version: ${MOJAP_IMAGE_VERSION}) ==="
@@ -15,13 +15,13 @@ if [ "${IS_SUNDAY_DEPLOY}" = "True" ]; then
   ./date-checker.sh
 fi
 
+echo "=== Running clone-create-a-derived-table.sh ==="
+./clone-create-a-derived-table.sh
+
 echo "=== Am I running a dataset check? ${IS_DATASET_CHECK} ==="
 if [ "${IS_DATASET_CHECK}" = "True" ]; then
   uv run check_run_results.py
 fi
-
-echo "=== Running clone-create-a-derived-table.sh ==="
-./clone-create-a-derived-table.sh
 
 echo "=== Running create-a-derived-table.sh ==="
 ./create-a-derived-table.sh
