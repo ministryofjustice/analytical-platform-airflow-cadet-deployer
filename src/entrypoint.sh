3e14a9f80e1d2b793fa2622eb7ec0610e4fe99e2
Jake H-P <72385707+jhpyke@users.noreply.github.com>
2026-02-23 15:55:24 +0000

    Update entrypoint to better handle `dataset_check` functionality (#82)

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,6 +6,26 @@ export IS_SUNDAY_DEPLOY="${IS_SUNDAY_DEPLOY:-false}"
 export MOJAP_IMAGE_VERSION="${MOJAP_IMAGE_VERSION:-"unknown"}"
 export IS_DATASET_CHECK="${IS_DATASET_CHECK:-"False"}"
 export DATASET_TARGET="${DATASET_TARGET:-""}"
+export UNIQUE_IDS="${UNIQUE_IDS:-}"
+
+function dataset_check() {
+  local base_command="uv run check_run_results.py"
+  if [ -z "${DATASET_TARGET}" ]; then
+    local command="${base_command} --check-all-nodes"
+  elif [ -n "${UNIQUE_IDS}" ]; then
+    local command="${base_command} --unique-ids \"${UNIQUE_IDS}\""
+  else
+    local command="${base_command}"
+  fi
+  echo "Running dataset check with command: ${command}"
+  if $command; then
+    echo "Dataset Check Passed!"
+    exit 0
+  else
+    echo "Dataset Check Failed! See Logs for Details"
+    exit 1
+  fi
+}
 
 echo "=== Running Airflow CaDeT Deployer (version: ${MOJAP_IMAGE_VERSION}) ==="
 
@@ -20,25 +40,7 @@ echo "=== Running clone-create-a-derived-table.sh ==="
 
 echo "=== Am I running a dataset check? ${IS_DATASET_CHECK} ==="
 if [ "${IS_DATASET_CHECK}" = "True" ]; then
-  if [ -z "${DATASET_TARGET}" ]; then
-    echo "=== Running dataset check for all nodes ==="
-    if uv run check_run_results.py --check-all-nodes; then
-      echo "Dataset Check Passed!"
-      exit 0
-    else
-      echo "Dataset Check Failed! See Logs for Details"
-      exit 1
-    fi
-  else
-    echo "=== Running dataset check for target: ${DATASET_TARGET} ==="
-    if uv run check_run_results.py; then
-      echo "Dataset Check Passed!"
-      exit 0
-    else
-      echo "Dataset Check Failed! See Logs for Details"
-      exit 1
-    fi
-  fi
+  dataset_check
 fi
 
 echo "=== Running create-a-derived-table.sh ==="
