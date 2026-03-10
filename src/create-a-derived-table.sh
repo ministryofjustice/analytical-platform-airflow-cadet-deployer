637848048a15b2b349435605722503dba596c4b3
Jovita Brundziene <jovita.brundziene@justice.gov.uk>
2026-03-10 15:24:22 +0000

    changed logic to opt out the sdp_table project from running export_run_artefacts.py

--- src/create-a-derived-table.sh
+++ src/create-a-derived-table.sh
@@ -131,20 +131,13 @@ function export_run_artefacts() {
   export RUN_TIME
   export AIRFLOW_WORKFLOW_REF="${WORKFLOW_NAME:-"unknown_workflow"}"
 
-  # Skip exporting run artefacts for specific projects if not needed
+  # Skip exporting run artefacts for sdp_tables projects
   if [ "${DBT_PROJECT}" = "sdp_tables" ]; then
     echo "Skipping export of run artefacts for project sdp_tables"
     return
-  fi
-
-  # For mojap_derived_tables project we call it directly
-  if [ "${DBT_PROJECT}" = "mojap_derived_tables" ]; then
+  else
     python "${REPOSITORY_PATH}/scripts/export_run_artefacts.py"
-    return
   fi
-
-  # For any other projects we currently do not export run artefacts
-  echo "No export_run_artefacts behaviour defined for project ${DBT_PROJECT}; skipping"
 }
 
 function import_run_artefacts() {
